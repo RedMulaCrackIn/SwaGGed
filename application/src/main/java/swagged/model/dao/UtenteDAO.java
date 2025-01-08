a503acd0964777725245c324d268775123d0dee4
Mattia Gallucci <m.gallucci18@studenti.unisa.it>
2025-01-08 19:31:01 +0100

    implementazione metodo update in UtenteDAO

--- application/src/main/java/swagged/model/dao/UtenteDAO.java
+++ application/src/main/java/swagged/model/dao/UtenteDAO.java
@@ -65,4 +65,37 @@ public class UtenteDAO {
         return result != 0;
     }
 
+    public synchronized boolean update(UtenteBean bean, String email) throws SQLException {
+        Connection con = null;
+        PreparedStatement statement = null;
+        int result = 0;
+
+        String query = "UPDATE " + TABLE_NAME + " SET username = ?, password = ?, immagine = ?, bandito = ?, admin = ? WHERE email = ?;";
+
+        try {
+            con = DriverManagerConnectionPool.getConnection();
+            statement = con.prepareStatement(query);
+
+            statement.setString(1, bean.getUsername());
+            statement.setString(2, bean.getPassword());
+            statement.setString(3, bean.getImmagine());
+            statement.setBoolean(4, bean.isBandito());
+            statement.setBoolean(5, bean.isAdmin());
+            statement.setString(6, email);
+
+            result = statement.executeUpdate();
+
+            con.commit();
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+            } finally {
+                DriverManagerConnectionPool.releaseConnection(con);
+            }
+        }
+        return result != 0;
+    }
+
 }
