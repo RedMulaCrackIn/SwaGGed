6a1e8ffb75c8af114682fc52ed8d9f0d78a303db
Mattia Gallucci <m.gallucci18@studenti.unisa.it>
2025-01-08 19:44:18 +0100

    aggiunta variabili nei controller commento

--- application/src/main/java/swagged/gestionecommenti/controller/VisualizzaCommentoServlet.java
+++ application/src/main/java/swagged/gestionecommenti/controller/VisualizzaCommentoServlet.java
@@ -2,7 +2,11 @@ package swagged.gestionecommenti.controller;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import swagged.gestionecommenti.services.GestioneCommentiServiceImpl;
 
 @WebServlet("/visualizzaCommento")
 public class VisualizzaCommentoServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static final GestioneCommentiServiceImpl gestioneCommenti = new GestioneCommentiServiceImpl();
+
 }
