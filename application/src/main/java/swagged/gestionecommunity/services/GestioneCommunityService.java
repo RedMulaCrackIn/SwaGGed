951cde02f9d78194af8078dd3ee9e965931da88f
RedMulaCrackIn <giony2003@gmail.com>
2025-01-09 11:09:48 +0100

    GestioneCommunityService - Implementazione firme

--- application/src/main/java/swagged/gestionecommunity/services/GestioneCommunityService.java
+++ application/src/main/java/swagged/gestionecommunity/services/GestioneCommunityService.java
@@ -1,4 +1,16 @@
 package swagged.gestionecommunity.services;
 
+import swagged.model.bean.CommunityBean;
+import swagged.model.bean.UtenteBean;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface GestioneCommunityService {
+    CommunityBean create(String nome, String descrizione, UtenteBean utente) throws SQLException;
+    boolean remove(CommunityBean community, UtenteBean utente) throws SQLException;
+    CommunityBean visualizza(String nome) throws SQLException;
+    CommunityBean iscrizione(UtenteBean utente, String nome) throws SQLException;
+    List<CommunityBean> cerca(String substring) throws SQLException;
+    boolean checkNome(String nome) throws SQLException;
 }
--- application/src/main/java/swagged/model/dao/IscrivitiCommunityDAO.java
+++ application/src/main/java/swagged/model/dao/IscrivitiCommunityDAO.java
@@ -8,7 +8,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+
 import java.util.List;
+
 public class IscrivitiCommunityDAO {
 
     private static final String TABLE_NAME = "iscrivitiCommunity";
