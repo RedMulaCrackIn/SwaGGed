782a3d7cc26c50edacd16738b008740c1e127b66
RedMulaCrackIn <giony2003@gmail.com>
2025-01-09 12:33:53 +0100

    VisualizzaUtenteServlet - metodo doGet

--- application/src/main/java/swagged/gestioneutenti/controller/VisualizzaUtenteServlet.java
+++ application/src/main/java/swagged/gestioneutenti/controller/VisualizzaUtenteServlet.java
@@ -1,8 +1,40 @@
 package swagged.gestioneutenti.controller;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import swagged.gestioneutenti.services.GestioneUtentiServiceImpl;
+import swagged.model.bean.UtenteBean;
+import swagged.model.dao.UtenteDAO;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/visualizzaUtente")
-public class VisualizzaUtenteServlet extends HttpServlet {
+public class VisualizzaUtenteServlet extends HttpServlet
+{
+    private static final long serialVersionUID = 1L;
+    private static final GestioneUtentiServiceImpl gestioneUtenti = new GestioneUtentiServiceImpl();
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        String username = request.getParameter("username");
+        UtenteBean profilo = null;
+        try {
+            profilo = gestioneUtenti.visualizza(username);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        request.getSession().setAttribute("profilo", profilo);
+        UtenteBean utente = (UtenteBean) request.getSession().getAttribute("utente");
+        if(utente == null)
+            response.sendRedirect(request.getContextPath() + "/utente.jsp");
+        else if(profilo.getUsername().equals(utente.getUsername())) {
+            response.sendRedirect(request.getContextPath() + "/common/profilo.jsp");
+        }else
+            response.sendRedirect(request.getContextPath() + "/utente.jsp");
+    }
+
 }
