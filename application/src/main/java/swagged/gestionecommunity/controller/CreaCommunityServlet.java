2ed3cb2b22ed0d1095dac36da8aa4ff846188d77
RedMulaCrackIn <giony2003@gmail.com>
2025-01-09 12:17:23 +0100

    CreaCommunityServlet - metodo doPost

--- application/src/main/java/swagged/gestionecommunity/controller/CreaCommunityServlet.java
+++ application/src/main/java/swagged/gestionecommunity/controller/CreaCommunityServlet.java
@@ -1,13 +1,35 @@
 package swagged.gestionecommunity.controller;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import swagged.gestionecommunity.services.GestioneCommunityService;
 import swagged.gestionecommunity.services.GestioneCommunityServiceImpl;
+import swagged.model.bean.CommunityBean;
+import swagged.model.bean.UtenteBean;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/creaCommunity")
 public class CreaCommunityServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final GestioneCommunityService gestioneCommunity = new GestioneCommunityServiceImpl();
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        String nome = request.getParameter("communityNomeCreazione");
+        String descrizione = request.getParameter("descrizione");
+        UtenteBean utente = (UtenteBean) request.getSession().getAttribute("utente");
+        CommunityBean community = null;
+        try {
+            community = gestioneCommunity.create(nome, descrizione, utente);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        request.getSession().setAttribute("utente", utente);
+        response.sendRedirect(request.getContextPath() + "/visualizzaCommunity?nome=" + community.getNome());
+    }
 }
