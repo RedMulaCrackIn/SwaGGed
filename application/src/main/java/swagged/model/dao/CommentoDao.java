872db6e407b163ef2c462ccf547835e39def6b5a
RedMulaCrackIn <giony2003@gmail.com>
2025-01-09 10:43:36 +0100

    Creazione iniziale del commento Dao con il metodo save

---
+++ application/src/main/java/swagged/model/dao/CommentoDao.java
@@ -0,0 +1,38 @@
+package swagged.model.dao;
+
+import swagged.model.bean.CommentoBean;
+import swagged.utils.DriverManagerConnectionPool;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+public class CommentoDao {
+    private static final String TABLE_NAME = "commento";
+
+    public synchronized boolean save(CommentoBean bean) throws SQLException {
+        Connection con = null;
+        PreparedStatement statement = null;
+        int result = 0;
+
+        String query = "INSERT INTO " + TABLE_NAME + " (corpo, utenteEmail, postId) VALUES (?,?,?)";
+
+        try {
+            con = DriverManagerConnectionPool.getConnection();
+            statement = con.prepareStatement(query);
+
+            statement.setString(1, bean.getCorpo());
+            statement.setString(2, bean.getUtenteEmail());
+            statement.setInt(3, bean.getPostId());
+
+            result = statement.executeUpdate();
+            con.commit();
+        } finally {
+            if (statement != null) statement.close();
+            DriverManagerConnectionPool.releaseConnection(con);
+        }
+        return result != 0;
+    }
+}
