4645f17bb068c3f5f755c1c386c28e49c74529b0
Mattia Gallucci <m.gallucci18@studenti.unisa.it>
2025-01-09 11:05:26 +0100

    specifica metodi in GestioneCommentiService

--- application/src/main/java/swagged/gestionecommenti/services/GestioneCommentiService.java
+++ application/src/main/java/swagged/gestionecommenti/services/GestioneCommentiService.java
@@ -1,4 +1,13 @@
 package swagged.gestionecommenti.services;
 
+import swagged.model.bean.CommentoBean;
+import swagged.model.bean.UtenteBean;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface GestioneCommentiService {
+    CommentoBean create(int postId, String corpo, String utenteEmail) throws SQLException;
+    boolean remove(int id, int postId, UtenteBean utente) throws SQLException;
+    List<CommentoBean> visualizza(int postId) throws SQLException;
 }
--- application/src/main/java/swagged/gestionecommenti/services/GestioneCommentiServiceImpl.java
+++ application/src/main/java/swagged/gestionecommenti/services/GestioneCommentiServiceImpl.java
@@ -1,4 +1,24 @@
 package swagged.gestionecommenti.services;
 
+import swagged.model.bean.CommentoBean;
+import swagged.model.bean.UtenteBean;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class GestioneCommentiServiceImpl implements GestioneCommentiService{
+    @Override
+    public CommentoBean create(int postId, String corpo, String utenteEmail) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean remove(int id, int postId, UtenteBean utente) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public List<CommentoBean> visualizza(int postId) throws SQLException {
+        return List.of();
+    }
 }
