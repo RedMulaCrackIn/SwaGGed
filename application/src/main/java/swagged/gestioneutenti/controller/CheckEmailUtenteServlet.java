1c0d493aedb8732df55344ebe5b3782757f4382d
RedMulaCrackIn <giony2003@gmail.com>
2025-01-09 12:41:54 +0100

    CheckEmailUtenteServlet - metodo doPost

--- application/src/main/java/swagged/gestioneutenti/controller/CheckEmailUtenteServlet.java
+++ application/src/main/java/swagged/gestioneutenti/controller/CheckEmailUtenteServlet.java
@@ -1,8 +1,41 @@
 package swagged.gestioneutenti.controller;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import swagged.gestioneutenti.services.GestioneUtentiServiceImpl;
+import swagged.model.dao.UtenteDAO;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/checkEmailUtente")
-public class CheckEmailUtenteServlet extends HttpServlet {
+public class CheckEmailUtenteServlet extends HttpServlet
+{
+    private static final long serialVersionUID = 1L;
+    private static final GestioneUtentiServiceImpl gestioneUtenti = new GestioneUtentiServiceImpl();
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String mode = request.getParameter("mode");
+        String path = null;
+        UtenteDAO utenteDAO = new UtenteDAO();
+
+        response.setContentType("text/plain");
+        String email = request.getParameter("email");
+
+        try {
+            if(gestioneUtenti.checkEmail(email)) {
+                response.getWriter().print("non disponibile");
+            } else {
+                response.getWriter().print("disponibile");
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
 }
