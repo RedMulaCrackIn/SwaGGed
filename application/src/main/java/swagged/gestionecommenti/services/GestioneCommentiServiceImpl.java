bc939f54a4348baf99ae8123f833032d5ccd6121
Mattia Gallucci <m.gallucci18@studenti.unisa.it>
2025-01-09 11:06:31 +0100

    implementazione metod create in GestioneCommentiServiceImpl

--- application/src/main/java/swagged/gestionecommenti/services/GestioneCommentiServiceImpl.java
+++ application/src/main/java/swagged/gestionecommenti/services/GestioneCommentiServiceImpl.java
@@ -1,16 +1,46 @@
 package swagged.gestionecommenti.services;
 
 import swagged.model.bean.CommentoBean;
+import swagged.model.bean.PostBean;
 import swagged.model.bean.UtenteBean;
+import swagged.model.dao.CommentoDAO;
+import swagged.model.dao.PostDAO;
+import swagged.model.dao.UtenteDAO;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class GestioneCommentiServiceImpl implements GestioneCommentiService{
+    private CommentoDAO commentoDAO = new CommentoDAO();
+    private UtenteDAO utenteDAO = new UtenteDAO();
+    private PostDAO postDAO = new PostDAO();
+
     @Override
     public CommentoBean create(int postId, String corpo, String utenteEmail) throws SQLException {
-        return null;
+        if(corpo == null || corpo.isEmpty() || utenteEmail == null || utenteEmail.isEmpty() || utenteDAO.getByEmail(utenteEmail) == null || postDAO.getById(postId) == null)
+            return null;
+
+        CommentoBean newCommento = new CommentoBean();
+        newCommento.setCorpo(corpo);
+        newCommento.setUtenteEmail(utenteEmail);
+        newCommento.setPostId(postId);
+
+        PostDAO postDAO = new PostDAO();
+        PostBean postBean = postDAO.getById(postId);
+        postBean.aumentaNumeroCommenti();
+        postBean.addCommento(newCommento);
+        postDAO.update(postBean);
+
+        UtenteBean utente = utenteDAO.getByEmail(utenteEmail);
+        utente.add("commentiCreati", newCommento);
+        postBean.addCommento(newCommento);
+
+        if(commentoDAO.save(newCommento))
+            return newCommento;
+        else
+            return null;
     }
+
 
     @Override
     public boolean remove(int id, int postId, UtenteBean utente) throws SQLException {
