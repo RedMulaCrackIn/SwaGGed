326e3bcb80dbd3a8738c6107f38fa82d3ec42e98
Mattia Gallucci <m.gallucci18@studenti.unisa.it>
2025-01-09 12:29:03 +0100

    implementazione gestione richiesta logout utente

--- application/src/main/java/swagged/gestioneutenti/controller/LogoutServlet.java
+++ application/src/main/java/swagged/gestioneutenti/controller/LogoutServlet.java
@@ -1,8 +1,21 @@
 package swagged.gestioneutenti.controller;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().removeAttribute("logged");
+        request.getSession().removeAttribute("utente");
+        request.getSession().invalidate();
+        response.sendRedirect(request.getContextPath() + "/homepage.jsp");
+    }
 }
