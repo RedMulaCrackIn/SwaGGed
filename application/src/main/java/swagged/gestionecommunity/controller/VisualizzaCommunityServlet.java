7bef2e2ad742851643cd00d1e271173d66e8c1f0
RedMulaCrackIn <giony2003@gmail.com>
2025-01-09 12:12:18 +0100

    VisualizzaCommunityServlet - metodo doGet

--- application/src/main/java/swagged/gestionecommunity/controller/VisualizzaCommunityServlet.java
+++ application/src/main/java/swagged/gestionecommunity/controller/VisualizzaCommunityServlet.java
@@ -1,13 +1,33 @@
 package swagged.gestionecommunity.controller;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import swagged.gestionecommunity.services.GestioneCommunityService;
 import swagged.gestionecommunity.services.GestioneCommunityServiceImpl;
+import swagged.model.bean.CommunityBean;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/visualizzaCommunity")
 public class VisualizzaCommunityServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final GestioneCommunityService gestioneCommunity = new GestioneCommunityServiceImpl();
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+
+            String nome = request.getParameter("nome");
+        CommunityBean community = null;
+        try {
+            community = gestioneCommunity.visualizza(nome);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        request.getSession().setAttribute("community", community);
+            response.sendRedirect(request.getContextPath() + "/community.jsp");
+    }
 }
