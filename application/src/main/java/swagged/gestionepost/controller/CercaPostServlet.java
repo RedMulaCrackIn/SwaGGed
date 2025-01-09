5c197bbc02babc7f38def3ed8a7b3319c5b5ba30
Mattia Gallucci <m.gallucci18@studenti.unisa.it>
2025-01-09 12:25:53 +0100

    implementazione gestione richiesta cerca post

--- application/src/main/java/swagged/gestionepost/controller/CercaPostServlet.java
+++ application/src/main/java/swagged/gestionepost/controller/CercaPostServlet.java
@@ -1,13 +1,34 @@
 package swagged.gestionepost.controller;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import swagged.gestionepost.services.GestionePostService;
 import swagged.gestionepost.services.GestionePostServiceImpl;
+import swagged.model.bean.PostBean;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/cercaPost")
 public class CercaPostServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final GestionePostService gestionePost = new GestionePostServiceImpl();
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String substring = request.getParameter("substring");
+        List<PostBean> risultati = null;
+        try {
+            risultati = gestionePost.cerca(substring);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        request.getSession().setAttribute("risultati", risultati);
+        response.sendRedirect(request.getContextPath() + "/ricerca.jsp");
+
+    }
 }
