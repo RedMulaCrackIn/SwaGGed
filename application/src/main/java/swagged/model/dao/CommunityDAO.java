aa47474ce240f381567596b17443672869167707
Mattia Gallucci <m.gallucci18@studenti.unisa.it>
2025-01-09 09:45:49 +0100

    creazione metodo update e getByNome in CommunityDAO

--- application/src/main/java/swagged/model/dao/CommunityDAO.java
+++ application/src/main/java/swagged/model/dao/CommunityDAO.java
@@ -5,6 +5,7 @@ import swagged.utils.DriverManagerConnectionPool;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CommunityDAO {
@@ -59,6 +60,58 @@ public class CommunityDAO {
             }
         }
         return result != 0;
+    }
+
+    public synchronized boolean update(CommunityBean bean) throws SQLException {
+        Connection con = null;
+        PreparedStatement statement = null;
+        int result = 0;
+
+        String query = "UPDATE " + TABLE_NAME + " SET descrizione = ?, iscritti = ?, utenteEmail = ? WHERE nome = ?;";
+
+        try {
+            con = DriverManagerConnectionPool.getConnection();
+            statement = con.prepareStatement(query);
+
+            statement.setString(1, bean.getDescrizione());
+            statement.setInt(2, bean.getIscritti());
+            statement.setString(3, bean.getUtenteEmail());
+            statement.setString(4, bean.getNome());
+
+            result = statement.executeUpdate();
+            con.commit();
+        } finally {
+            if (statement != null) statement.close();
+            DriverManagerConnectionPool.releaseConnection(con);
+        }
+        return result != 0;
+    }
+
+    public synchronized CommunityBean getByNome(String nome) throws SQLException {
+        Connection con = null;
+        PreparedStatement statement = null;
+        CommunityBean community = null;
+
+        String query = "SELECT * FROM " + TABLE_NAME + " WHERE nome = ?;";
+
+        try {
+            con = DriverManagerConnectionPool.getConnection();
+            statement = con.prepareStatement(query);
+            statement.setString(1, nome);
+
+            ResultSet result = statement.executeQuery();
+            if (result.next()) {
+                community = new CommunityBean();
+                community.setNome(result.getString("nome"));
+                community.setDescrizione(result.getString("descrizione"));
+                community.setIscritti(result.getInt("iscritti"));
+                community.setUtenteEmail(result.getString("utenteEmail"));
+            }
+        } finally {
+            if (statement != null) statement.close();
+            DriverManagerConnectionPool.releaseConnection(con);
+        }
+        return community;
     }
 
 }
