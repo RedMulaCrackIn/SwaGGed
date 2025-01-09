f2bd028ff2a212cdeeaf4c2d70b4f10d99e263e4
RedMulaCrackIn <giony2003@gmail.com>
2025-01-09 10:54:43 +0100

    IscrivitiCommunityDAO - metodo save

--- application/src/main/java/swagged/model/dao/IscrivitiCommunityDAO.java
+++ application/src/main/java/swagged/model/dao/IscrivitiCommunityDAO.java
@@ -1,4 +1,42 @@
 package swagged.model.dao;
 
+import swagged.model.bean.IscrivitiCommunityBean;
+import swagged.utils.DriverManagerConnectionPool;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 public class IscrivitiCommunityDAO {
+
+    private static final String TABLE_NAME = "iscrivitiCommunity";
+
+    public synchronized boolean save(IscrivitiCommunityBean bean) throws SQLException {
+        Connection con = null;
+        PreparedStatement statement = null;
+        int result = 0;
+
+        String query = "INSERT INTO " + TABLE_NAME + " (utenteEmail, communityNome) VALUES (?,?)";
+
+        try{
+            con = DriverManagerConnectionPool.getConnection();
+            statement = con.prepareStatement(query);
+
+            statement.setString(1, bean.getUtenteEmail());
+            statement.setString(2, bean.getCommunityNome());
+
+            result = statement.executeUpdate();
+            con.commit();
+        } finally {
+            try {
+                if (statement != null)
+                    statement.close();
+            } finally {
+                DriverManagerConnectionPool.releaseConnection(con);
+            }
+        }
+        return result != 0;
+    }
 }
