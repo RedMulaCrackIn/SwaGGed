8162cd2951f83e8c2df2654c3f5082993306169d
Mattia Gallucci <m.gallucci18@studenti.unisa.it>
2025-01-09 09:46:41 +0100

    creazione metodo getAll, getByEmail e getByNameSubstring in CommunityDAO

--- application/src/main/java/swagged/model/dao/CommunityDAO.java
+++ application/src/main/java/swagged/model/dao/CommunityDAO.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommunityDAO {
     private static final String TABLE_NAME = "community";
@@ -112,6 +114,105 @@ public class CommunityDAO {
             DriverManagerConnectionPool.releaseConnection(con);
         }
         return community;
+    }
+
+    public synchronized List<CommunityBean> getAll() throws SQLException {
+        Connection con = null;
+        PreparedStatement statement = null;
+        List<CommunityBean> communities = new ArrayList<>();
+
+        String query = "SELECT * FROM " + TABLE_NAME;
+
+        try {
+            con = DriverManagerConnectionPool.getConnection();
+            statement = con.prepareStatement(query);
+
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+                CommunityBean community = new CommunityBean();
+                community.setNome(result.getString("nome"));
+                community.setDescrizione(result.getString("descrizione"));
+                community.setIscritti(result.getInt("iscritti"));
+                community.setUtenteEmail(result.getString("utenteEmail"));
+
+                communities.add(community);
+            }
+        } finally {
+            if (statement != null) statement.close();
+            DriverManagerConnectionPool.releaseConnection(con);
+        }
+        return communities;
+    }
+
+    public synchronized List<CommunityBean> getByEmail(String email) throws SQLException {
+        Connection con = null;
+        PreparedStatement statement = null;
+        List<CommunityBean> communities = new ArrayList<>();
+
+        String query = "SELECT * FROM " + TABLE_NAME + " WHERE utenteEmail = ?;";
+
+        try {
+            con = DriverManagerConnectionPool.getConnection();
+            statement = con.prepareStatement(query);
+            statement.setString(1, email);
+
+            ResultSet result = statement.executeQuery();
+
+            while (result.next()) {
+                CommunityBean community = new CommunityBean();
+                community.setNome(result.getString("nome"));
+                community.setDescrizione(result.getString("descrizione"));
+                community.setIscritti(result.getInt("iscritti"));
+                community.setUtenteEmail(result.getString("utenteEmail"));
+
+                communities.add(community);
+            }
+        } finally {
+            try {
+                if (statement != null)
+                    statement.close();
+            } finally {
+                DriverManagerConnectionPool.releaseConnection(con);
+            }
+        }
+        return communities;
+    }
+
+    public synchronized List<CommunityBean> getByNameSubstring(String substring) throws SQLException {
+        Connection con = null;
+        PreparedStatement statement = null;
+        List<CommunityBean> communities = new ArrayList<>();
+
+        // Use the SQL LIKE operator for substring matching
+        String query = "SELECT * FROM " + TABLE_NAME + " WHERE nome LIKE ?;";
+
+        try {
+            con = DriverManagerConnectionPool.getConnection();
+            statement = con.prepareStatement(query);
+
+            // Prepare the substring for the SQL query, adding wildcards for partial matches
+            statement.setString(1, "%" + substring + "%");
+
+            ResultSet result = statement.executeQuery();
+
+            while (result.next()) {
+                CommunityBean community = new CommunityBean();
+                community.setNome(result.getString("nome"));
+                community.setDescrizione(result.getString("descrizione"));
+                community.setIscritti(result.getInt("iscritti"));
+                community.setUtenteEmail(result.getString("utenteEmail"));
+
+                communities.add(community);
+            }
+        } finally {
+            try {
+                if (statement != null)
+                    statement.close();
+            } finally {
+                DriverManagerConnectionPool.releaseConnection(con);
+            }
+        }
+        return communities;
     }
 
 }
