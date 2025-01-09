1cee9922c6d1fd4b47cbc5d7c41826307001b73f
RedMulaCrackIn <giony2003@gmail.com>
2025-01-09 11:26:14 +0100

    GestioneUtentiServiceImpl - implementazione firme

--- application/src/main/java/swagged/gestioneutenti/services/GestioneUtentiService.java
+++ application/src/main/java/swagged/gestioneutenti/services/GestioneUtentiService.java
@@ -1,4 +1,20 @@
 package swagged.gestioneutenti.services;
 
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.http.Part;
+import swagged.model.bean.UtenteBean;
+
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface GestioneUtentiService {
+    boolean ban (String email) throws SQLException;
+    List<UtenteBean> cerca(String substring) throws SQLException;
+    boolean modificaImmagine(UtenteBean utente, Part filePart, GenericServlet servlet) throws SQLException;
+    boolean checkEmail(String email) throws SQLException;
+    boolean checkUsername(String username) throws SQLException;
+    UtenteBean visualizza(String username) throws SQLException;
+    UtenteBean login(String username, String password) throws SQLException;
+    UtenteBean registrazione(String email, String username, String password, String passwordCheck) throws SQLException;
 }
